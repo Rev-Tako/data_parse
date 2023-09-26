71481f3e428fc1b5c0442a860875e0835e0a27b4
RevTako <turo15@icloud.com>
2023-09-26 14:08:21 +0100

    updated to red 10-digit codes

--- scarlet_parse.py
+++ scarlet_parse.py
@@ -6,8 +6,10 @@ from os.path import exists
 
 
 def readfile(directoryname, regfile):
-    # filepath = f'{directoryname}/[0-9][0-9][0-9][0-9][0-9][0-9]{regfile}*'
-    filepath = f'{directoryname}/[0-9][0-9][0-9][0-9][0-9]{regfile}*'
+    # filepath = f'{directoryname}/[0-9][0-9][0-9][0-9][0-9][0-9][0-9]{regfile}*'  # 7
+    # filepath = f'{directoryname}/[0-9][0-9][0-9][0-9][0-9][0-9]{regfile}*'  # 6
+    # filepath = f'{directoryname}/[0-9][0-9][0-9][0-9][0-9]{regfile}*'  # 5
+    filepath = f'{directoryname}/[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]{regfile}*'  # 10
     print(f'filepattern expected:  {filepath}')
     txt = glob.glob(filepath)
     txt.sort()
@@ -24,11 +26,11 @@ def readfile(directoryname, regfile):
         outfile = filename.replace(f'{regfile}*.txt', '').replace(f'{directoryname}/', '').replace(f'.txt', '')[0:6]
         outline = filename.replace(f'{directoryname}/', '')
         with open(filename, 'r') as f:
-            print(f'creating output file at: output_csvs/outfile_2.csv')
+            print(f'creating output file at: output_csvs/outfile_updated_2.csv')
             if not os.path.isdir('output_csvs'):
                 os.mkdir('output_csvs')
-            with open(f'output_csvs/outfile_2.csv', 'a+') as fileout:
-                print(f'reading: {outline}, saving to outfile_2.csv')
+            with open(f'output_csvs/outfile_updated_2.csv', 'a+') as fileout:
+                print(f'reading: {outline}, saving to outfile_updated_2.csv')
                 fileout.seek(0)
                 if outline in fileout.read():
                     break
